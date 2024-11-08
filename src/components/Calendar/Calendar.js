d1ab0285e2f26797779c32c8d178d2c778c8b25e
Oleksandra Tikka <tikkasandy@gmail.com>
2024-11-08 19:27:58 +0200

    test

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -6,16 +6,19 @@ import s from "./Calendar.module.scss";
 const Calendar = () => {
   return (
     <ul className={s.Calendar}>
-      {adventDays.map((day) => (
-        <li key={day.date} className={`${s.Day} ${s[day.color]}`}>
-          <Link to={`/${day.date}`} className={s.Link}>
-            <p className={`${s.Date} ${s[day.datePosition]}`}>{day.date}</p>
-            <svg className={`${s.Logo} ${s[day.size]} ${s[day.logoPosition]}`}>
-              <use href={sprite + `#icon-svg${day.date}`} />
-            </svg>
-          </Link>
-        </li>
-      ))}
+      {adventDays.map((day) => {
+          return (
+              <li key={day.date} className={`${s.Day} ${s[day.color]}`}>
+                  <Link to={`/${day.date}`} className={s.Link}>
+                      <p className={`${s.Date} ${s[day.datePosition]}`}>{day.date}</p>
+                      <svg className={`${s.Logo} ${s[day.size]} ${s[day.logoPosition]}`}>
+                          <use href={sprite + `#icon-svg${day.date}`} />
+{/* test */}
+                      </svg>
+                  </Link>
+              </li>
+          );
+      })}
     </ul>
   );
 };
